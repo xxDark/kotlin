a00b5ce87fde892663be517083d70f24dff775cb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-07-23 10:27:39 +0300

    [FIR-TEST] Update testdata after previous commit

--- compiler/testData/diagnostics/tests/suspendConversion/basicSuspendConversionForCallableReference.fir.kt
+++ compiler/testData/diagnostics/tests/suspendConversion/basicSuspendConversionForCallableReference.fir.kt
@@ -9,10 +9,10 @@ fun bar2(x: Int) {}
 fun bar2(s: String) {}
 
 fun test() {
-    <!INAPPLICABLE_CANDIDATE!>foo1<!>(::bar1)
+    <!INAPPLICABLE_CANDIDATE!>foo1<!>(<!UNRESOLVED_REFERENCE!>::bar1<!>)
 
     <!INAPPLICABLE_CANDIDATE!>foo2<!>(42, <!UNRESOLVED_REFERENCE!>::bar2<!>)
     <!INAPPLICABLE_CANDIDATE!>foo2<!>("str", <!UNRESOLVED_REFERENCE!>::bar2<!>)
 
-    <!INAPPLICABLE_CANDIDATE!>foo2<!>(42, ::bar1)
+    <!INAPPLICABLE_CANDIDATE!>foo2<!>(42, <!UNRESOLVED_REFERENCE!>::bar1<!>)
 }
