2a23a6cc25a281b18cd8960a3879f55600a7f61e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-07-27 13:09:42 +0300

    [FIR] Don't report duplicated errors on default accessor error type refs

--- compiler/testData/diagnostics/testsWithStdLib/InaccessibleInternalClass.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/InaccessibleInternalClass.fir.kt
@@ -10,6 +10,6 @@ package kotlin.sequences
 import p.*
 
 interface I {
-    val v1: <!OTHER_ERROR, OTHER_ERROR!>FilteringSequence<!>
+    val v1: <!OTHER_ERROR!>FilteringSequence<!>
     val v2: IndexingSequence<String>
 }
