fbda473a368c6234dedbee80f8d243b2f0cce571
Arsen Nagdalian <arsen.nagdalian@jetbrains.com>
2020-07-27 15:44:15 +0300

    [FIR] Add a Fir node representing a contract function

--- compiler/fir/tree/gen/org/jetbrains/kotlin/fir/visitors/FirVisitorVoid.kt
+++ compiler/fir/tree/gen/org/jetbrains/kotlin/fir/visitors/FirVisitorVoid.kt
@@ -127,6 +127,7 @@ import org.jetbrains.kotlin.fir.types.FirFunctionTypeRef
 import org.jetbrains.kotlin.fir.types.FirResolvedFunctionTypeRef
 import org.jetbrains.kotlin.fir.types.FirImplicitTypeRef
 import org.jetbrains.kotlin.fir.types.FirComposedSuperTypeRef
+import org.jetbrains.kotlin.fir.declarations.FirContractFunction
 import org.jetbrains.kotlin.fir.contracts.FirEffectDeclaration
 import org.jetbrains.kotlin.fir.contracts.FirContractDescription
 import org.jetbrains.kotlin.fir.contracts.FirLegacyRawContractDescription
@@ -623,6 +624,10 @@ abstract class FirVisitorVoid : FirVisitor<Unit, Nothing?>() {
 
     open fun visitComposedSuperTypeRef(composedSuperTypeRef: FirComposedSuperTypeRef) {
         visitElement(composedSuperTypeRef)
+    }
+
+    open fun visitContractFunction(contractFunction: FirContractFunction) {
+        visitElement(contractFunction)
     }
 
     open fun visitEffectDeclaration(effectDeclaration: FirEffectDeclaration) {
@@ -1131,6 +1136,10 @@ abstract class FirVisitorVoid : FirVisitor<Unit, Nothing?>() {
 
     final override fun visitComposedSuperTypeRef(composedSuperTypeRef: FirComposedSuperTypeRef, data: Nothing?) {
         visitComposedSuperTypeRef(composedSuperTypeRef)
+    }
+
+    final override fun visitContractFunction(contractFunction: FirContractFunction, data: Nothing?) {
+        visitContractFunction(contractFunction)
     }
 
     final override fun visitEffectDeclaration(effectDeclaration: FirEffectDeclaration, data: Nothing?) {
